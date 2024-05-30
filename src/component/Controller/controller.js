3b24ffeb33ad3b32386e84053f3ef93bb21b41aa
Mikeil Akovity (1iekim) <mikeil98@mail.ru>
2024-05-30 11:14:25 +0300

    fix: fix fetch path

--- src/component/Controller/controller.js
+++ src/component/Controller/controller.js
@@ -10,31 +10,18 @@ class AppController extends AppLoader {
     );
   }
 
-  getNews(callback, btnId="") {
+  getNews(callback, btnId = "") {
     const options = {};
-    if(btnId.length >= 1) {
+    if (btnId.length >= 1) {
       options.sources = btnId;
     }
-    // let target = e.target;
-    // const newsContainer = e.currentTarget;
-
-    // while (target !== newsContainer) {
-    //   if (target.classList.contains("source__item")) {
-    //     const sourceId = target.getAttribute("data-source-id") ?? "";
-    //     if (newsContainer.getAttribute("data-source") !== sourceId) {
-    //       newsContainer.setAttribute("data-source", sourceId);
-          super.getResp(
-            {
-              endpoint: "everything",
-              options: options,
-            },
-            callback
-          );
-        // }
-        // return;
-      // }
-      // target = target.parentNode;
-    // }
+    super.getResp(
+      {
+        endpoint: "everything",
+        options: options,
+      },
+      callback
+    );
   }
 }
 
